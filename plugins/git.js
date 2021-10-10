0865c4a9c72cb0633c83f18bf0c16f9232509db8
sudev7 <92219207+sudev7@users.noreply.github.com>
2021-10-10 11:34:21 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://www.linkpicture.com/q/malooty.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is sudev pv*
+*Creator number : wa.me/919946432377?text=Hi%20sudev%20pv.%20*
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Githublink (Setup)  :    https://github.com/sudev7/malooty*
+*Audio commads :   https://github.com/sudev7/malooty/tree/master/uploads*
+*Sticker commads : https://github.com/sudev7/malooty/tree/master/uploads*
 `}) 
 
 }));
